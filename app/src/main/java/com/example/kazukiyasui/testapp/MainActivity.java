d732f074f75c694c7681de07a367d61db957f71b
yasu0905 <35673575+yasu0905@users.noreply.github.com>
2018-07-24 14:39:40 +0900

    こみっとしちゃうぞ

--- app/src/main/java/com/example/kazukiyasui/testapp/MainActivity.java
+++ app/src/main/java/com/example/kazukiyasui/testapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
@@ -38,11 +39,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //「3」ボタンのクリックリスナーセット
         Button btn_red = findViewById(R.id.btn_Red);
         btn_red.setOnClickListener(this);
+
+
     }
 
     @Override
     public void onClick(View view) {
+        ImageView iv_droid = findViewById(R.id.iv_droid);
 
+        switch (view.getId()){
+            case R.id.btn_Green:
+                iv_droid.setImageResource(R.drawable.android_logo);
+                break;
+            case R.id.btn_Blue:
+                iv_droid.setImageResource(R.drawable.android_logo_blue);
+                break;
+            case R.id.btn_Red:
+                iv_droid.setImageResource(R.drawable.android_logo_red);
+                break;
+        }
     }
 
 //    class OkButtonClickListener implements View.OnClickListener
